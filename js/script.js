e1408da8ff5a51c96fc94d62a47cc7866c93f534
lorrainekamanda <lorrainekamanda@gmail.com>
2020-03-22 18:12:51 +0300

    update js file

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ $("#first").hover (function(event){
     event.preventDefault()
 $("#welcome").slideToggle(1500)   
 })
+$("footer").hover (function(event){
+    event.preventDefault()
+$(".con").show(100)   
+})
+
